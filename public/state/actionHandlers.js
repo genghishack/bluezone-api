0abefc9e7e948915f9b241e184ed26c48a801660
Chris Wade <chrisw@brightervision.com>
2020-10-26 00:40:44 -0600

    Added endpoint for getting bboxes

--- public/state/actionHandlers.js
+++ public/state/actionHandlers.js
@@ -18,9 +18,31 @@ async function getDistrictsByState() {
   }
 }
 
+async function getStateAndDistrictBBoxes() {
+  let result = {};
+  let bboxes = {};
+  try {
+    result = await stateLib.getStateAndDistrictBBoxes();
+    // console.log('state and district bboxes: ', result);
+    const [ row ] = result;
+    const { state_bboxes: stateBBoxes, district_bboxes: districtBBoxes} = row;
+    stateBBoxes.forEach(result => {
+      bboxes[result.usps] = result.bbox;
+    })
+    districtBBoxes.forEach(result => {
+      bboxes[`${result.usps}${result.district_num}`] = result.bbox;
+    })
+    return success({data: bboxes, count: 1});
+  } catch (e) {
+    logError(e);
+    return failure({message: e.message});
+  }
+}
+
 const actionHandlers = {
   GET: {
     districts: getDistrictsByState,
+    bboxes: getStateAndDistrictBBoxes,
   },
 };
 
